9f263170ce05d52cf6e1c04bba2a7f1955a89771
孔庆杨 <739292435@qq.com>
2020-03-11 18:14:50 +0800

    添加爬虫

--- python高级用法/课程配套资料/chapter04/contextlib_with.py
+++ python高级用法/课程配套资料/chapter04/contextlib_with.py
@@ -1,10 +1,12 @@
 import contextlib
 
+
 @contextlib.contextmanager
 def file_open(file_name):
-    print ("file open")
+    print("file open")
     yield {}
-    print ("file end")
+    print("file end")
+
 
 with file_open("bobby.txt") as f_opened:
-    print ("file processing")
+    print("file processing")
