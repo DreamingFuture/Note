f32d3ba3cde2ccecd222e08891896e3bc9cdeef7
孔庆杨 <739292435@qq.com>
2020-04-20 11:08:42 +0800

    完善demo

--- python高级用法/装饰器.py
+++ python高级用法/装饰器.py
@@ -1,16 +1,67 @@
-def display_time(func):
-	def wrapper():
-		t1 = time.time()
-		func()
-		t2 = time.time()
-		print(t2 - t1)
+import time
 
 
-@display_time
-def main():
-    for i in range(1000):
+def display_time1(func):
+    def wrapper():
+        t1 = time.time()
+        func()
+        t2 = time.time()
+        print(t2 - t1)
+
+    return wrapper
+
+
+def display_time2(func):
+    def wrapper():
+        t1 = time.time()
+        result = func()
+        t2 = time.time()
+        print(t2 - t1)
+        return result
+
+    return wrapper
+
+
+def display_time3(func):
+    def wrapper(*args):
+        t1 = time.time()
+        result = func(*args)
+        t2 = time.time()
+        print(t2 - t1)
+        return result
+
+    return wrapper
+
+
+@display_time1
+def test1():
+    for i in range(1000000):
         pass
-    print('Hello world')
+    print("hello world!")
 
-if __name__ == '__main__':
-    main()
+
+@display_time2
+def test2():
+    for i in range(1000000):
+        pass
+    print("hello world!")
+    return 222
+
+
+@display_time3
+def test3(num1, num2):
+    for i in range(1000000):
+        pass
+    print(num2 - num1)
+    return 333
+
+
+test1()
+print('-' * 10)
+
+a = test2()
+print(a)
+print('-' * 10)
+
+b = test3(1, 0)
+print(b)
